da55d739ee5cd7f2c01f75918a7f32aac3358774
Pedro Rogério <pinceladasdaweb@hotmail.com>
2019-02-16 15:21:59 -0200

    Added profiles on routes

--- src/routes/users/users.router.ts
+++ src/routes/users/users.router.ts
@@ -32,9 +32,9 @@ class UsersRouter extends ModelRouter<User> {
   applyRoutes(application: restify.Server) {
     application.get(`${this.basePath}`, [this.findByEmail, this.findAll])
     application.get(`${this.basePath}/:id`, [this.validateId, this.findById])
-    application.post(`${this.basePath}`, [authorize('admin'), this.save])
-    application.put(`${this.basePath}/:id`, [authorize('admin'), this.validateId,this.replace])
-    application.patch(`${this.basePath}/:id`, [authorize('admin'), this.validateId,this.update])
+    application.post(`${this.basePath}`, [authorize('admin', 'user'), this.save])
+    application.put(`${this.basePath}/:id`, [authorize('admin', 'user'), this.validateId,this.replace])
+    application.patch(`${this.basePath}/:id`, [authorize('admin', 'user'), this.validateId,this.update])
     application.del(`${this.basePath}/:id`, [authorize('admin'), this.validateId,this.delete])
 
     application.post(`${this.basePath}/authenticate`, authenticate)
