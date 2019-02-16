e37274817172ef0173d37de8708a25e68792bdd9
Pedro Rogério <pinceladasdaweb@hotmail.com>
2019-02-15 22:42:15 -0200

    Added Bearer token for tests

--- jest.config.js
+++ jest.config.js
@@ -13,6 +13,7 @@ module.exports = {
     "node"
   ],
   globals: {
-    "address": "http://localhost:3001"
+    "address": "http://localhost:3001",
+    "auth": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhZG1pbkBlbWFpbC5jb20iLCJpYXQiOjE1MTYyMzkwMjJ9.HCtLeNy5-33THRqf03ayTWV0UiTNEuBgKzE-G_03d3g"
   }
 };
